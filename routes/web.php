b118c361c8d2c055ab82a6baba3ad0d1aa6d53e4
BigTechnologie <dev.technologie2018@gmail.com>
2025-04-04 10:15:06 +0200

    Laravel 11

---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Models\Blog;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController; 
+use App\Http\Controllers\SingleActionController;
+use App\Http\Controllers\BlogController;
+
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [HomeController::class, 'about']);
+Route::get('/single-action', SingleActionController::class); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 6_Routes de secours (erreur 404)
+Route::fallback(function () {
+    return "Ooops cette page n'existe pas !";
+});
+
+
+
+
+
+
+
+
